6a30b56886fa4c2ab0984bcabb89a7bef134dbaf
Arthur Reutenauer <arthur@reutenauer.eu>
2017-09-14 20:23:23 +0200

    wip

--- sdl/talgoxe/views.py
+++ sdl/talgoxe/views.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 from tempfile import mkdtemp
 from os import system, chdir
+import io
+import os
 
 from django.shortcuts import render
 
@@ -18,50 +20,40 @@ def index(request):
 def stickord(request, stickord):
     return common(request, stickord)
 
-def print_stickord(request, stickord):
-    lemma = Lemma.objects.filter(lemma = stickord).first()
+def doprint(request, stickord):
+
+def print_stuff(request, stickord = None):
     tempdir = mkdtemp('', 'SDLartikel')
-    sourcename = tempdir + '/sdl-artikel.tex'
+    sourcename = tempdir + '/sdl.tex'
     source = open(sourcename, 'w')
     source.write("""
         \\setupbodyfont[pagella, 12pt]
         \\setuppagenumbering[state=stop]
 
-        \\catcode`\\:=11
-        \\catcode`\\Ö=11
-        \\def\\SDL:SO#1{{\\bf #1}}
-        \\def\\SDL:OK#1{{\\bfx #1}}
-        \\def\\SDL:KO#1{}
-        \\def\\SDL:G#1{{\\tfx #1}}
-        \\def\\SDL:DSP#1{{\\it #1}}
-        \\def\\SDL:SP#1{{\\it #1}}
-        \\def\\SDL:TIP#1{{\\tfx #1}}
-        \\def\\SDL:IP#1{{\\kern-0.3em #1}}
-        \\def\\SDL:M#1#2{{\\bf #2}}
-        \\def\\SDL:M#1#2{{\\bf #2}}
-        \\def\\SDL:VH#1{{#1\\kern-0.3em}}
-        \\def\SDL:HH#1{{\\kern-0.3em #1}}
-        \\def\\SDL:VR#1{{#1\\kern-0.3em}}
-        \\def\\SDL:HR#1{{\\kern-0.3em #1}}
-        \\def\\SDL:REF#1{{\\bfx #1}}
-        \\def\\SDL:FO#1{{\\it #1}}
-        \\def\\SDL:TIK#1{{\\tfx\\it #1}}
-        \\def\\SDL:FLV#1{{\\bfx #1}}
-        \\def\\SDL:ÖVP#1{{(#1)}}
-        \\def\\SDL:BE#1{#1}
-        \\def\SDL:ÖV#1{#1}
+        """)
+    with io.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'lib', 'sdl-setup.tex')) as file:
+        source.write(file.read().encode('UTF-8'))
 
+    source.write("""
         \\starttext
         """.encode('UTF-8'))
-    lemma.process(source)
+
+    if stickord:
+        lemma = Lemma.objects.filter(lemma = stickord).first()
+        lemma.process(source)
+    else:
+        for lemma in Lemma.objects.filter(id__gt = 0).order_by('lemma'):
+              lemma.process(source)
+
     source.write("\\stoptext\n")
     source.close()
     chdir(tempdir)
-    system("context sdl-artikel.tex")
-    # return HttpResponse("ConTeXt has run!")
-    # return HttpResponse(sourcename)
+    system("context sdl.tex")
     return HttpResponse("<p><a href='file://%s'>Click here</a></p>" % sourcename.replace(r'.tex', '.pdf'))
-    # return HttpResponse("<p><a href='file://%s</a></p>" % sourcename.replace(r'.tex', '.pdf'))
+
+def print_stickord(request, stickord):
+
+def print_lexicon(request):
 
 def common(request, stickord):
     template = loader.get_template('talgoxe/index.html')
