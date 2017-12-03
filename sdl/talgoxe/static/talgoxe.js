0df817e1c85690befa9f4a989c989566dd51aa2c
Arthur Reutenauer <arthur@reutenauer.eu>
2017-12-03 22:47:15 +0100

    wip Making something good here ...

--- sdl/talgoxe/static/talgoxe.js
+++ sdl/talgoxe/static/talgoxe.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
+    names = {
+    }
+
     function addRow(event) {
         console.log(event.currentTarget.id);
         dpos = event.currentTarget.id.replace('button-', '');
-        $('#data-' + dpos).after('<li><input type="text" size="3"> <input type="text" size="16"> <button><strong>+</strong></button></li>');
+        if (!names[dpos]) { names[dpos] = 0 }
+        $('#data-' + dpos).after('<li><input type="text" size="3" name="type-' + dpos + '.' + names[dpos] + '> <input type="text" size="16"> <button><strong>+</strong></button></li>');
         console.log("Adding a row after d.pos " + dpos + " ...");
     }
 
