381708194cbeba54ed9efa77f369068c33cf21ed
Arthur Reutenauer <arthur@reutenauer.eu>
2017-12-03 22:37:07 +0100

    Starting to have fun!

--- sdl/talgoxe/static/talgoxe.js
+++ sdl/talgoxe/static/talgoxe.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
     function addRow(event) {
         console.log(event.currentTarget.id);
         dpos = event.currentTarget.id.replace('button-', '');
-        console.log("Adding a row after d.pos" + dpos + " ...");
+        /* console.log(event.currentTarget.parent); */
+        $('#data-' + dpos).after('<li><input type="text" size="3"><input type="text" size="16"></li>');
+        console.log("Adding a row after d.pos " + dpos + " ...");
     }
 
     $('.addRow').click(function(event) {
