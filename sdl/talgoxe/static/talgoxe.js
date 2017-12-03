4aaeb7d2d532b5f90f1fa5378c52abfd1d7b1785
Arthur Reutenauer <arthur@reutenauer.eu>
2017-12-03 22:08:52 +0100

    Cleanup.

--- sdl/talgoxe/static/talgoxe.js
+++ sdl/talgoxe/static/talgoxe.js
@@ -1,16 +1,8 @@
 $(document).ready(function() {
     function addRow(event) {
-        console.log("Adding a row ...");
-        console.log($);
-        console.log($.id);
-        console.log($.attr('id'));
-        /* console.log($.id()); */
-        console.log(event);
-        console.log(this.id);
-        console.log(this);
-        console.log(event.currentTarget);
         console.log(event.currentTarget.id);
-        console.log(event.currentTarget.id.replace('button-', ''));
+        dpos = event.currentTarget.id.replace('button-', '');
+        console.log("Adding a row after d.pos" + dpos + " ...");
     }
 
     $('.addRow').click(function(event) {
