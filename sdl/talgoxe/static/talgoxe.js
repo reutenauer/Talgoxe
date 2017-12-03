d3da53bdb1d78537aa10a2e66b20701f02978248
Arthur Reutenauer <arthur@reutenauer.eu>
2017-12-03 22:39:25 +0100

    Cleanup.

--- sdl/talgoxe/static/talgoxe.js
+++ sdl/talgoxe/static/talgoxe.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     function addRow(event) {
         console.log(event.currentTarget.id);
         dpos = event.currentTarget.id.replace('button-', '');
-        /* console.log(event.currentTarget.parent); */
         $('#data-' + dpos).after('<li><input type="text" size="3"><input type="text" size="16"></li>');
         console.log("Adding a row after d.pos " + dpos + " ...");
     }
