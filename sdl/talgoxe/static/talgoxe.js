e6e04efce9ea67b5907f7d98e0ecd30a3506f7a0
Arthur Reutenauer <arthur@reutenauer.eu>
2017-12-03 23:09:08 +0100

    Nice try, but that can’t work, for at least two reasons ...

--- sdl/talgoxe/static/talgoxe.js
+++ sdl/talgoxe/static/talgoxe.js
@@ -6,10 +6,12 @@ $(document).ready(function() {
         console.log(event.currentTarget.id);
         dpos = event.currentTarget.id.replace('button-', '');
         if (!names[dpos]) { names[dpos] = 0 }
-        newRowId = 'type-' + dpos + '.' + names[dpos]
+        newRowId = dpos + '.' + names[dpos]
         names[dpos]++
-        $('#data-' + dpos).after('<li><input type="text" size="3" name="' + newRowId + '> <input type="text" size="16"> <button><strong>+</strong></button></li>');
         console.log("Adding a row after d.pos " + dpos + " ...");
+        $('#data-' + dpos).after('<li id="data-' + newRowId + '"><input type="text" size="3" name="type-' + newRowId + '"> <input type="text" size="16" name="value-' + newRowId + '"> <button class="addRow" id="button-' + newRowId + '"><strong>+</strong></button></li>');
+        console.log("Registering the event");
+        $('#button-' + newRowId).click(function(event) { event.preventDefault(); addRow(event); });
     }
 
     $('.addRow').click(function(event) {
