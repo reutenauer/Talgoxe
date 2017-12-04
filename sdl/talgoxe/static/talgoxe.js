0db001b092fcfeabf3523e7ef584705f7120bd01
Arthur Reutenauer <arthur@reutenauer.eu>
2017-12-04 23:19:40 +0100

    Really nice now!

--- sdl/talgoxe/static/talgoxe.js
+++ sdl/talgoxe/static/talgoxe.js
@@ -2,17 +2,15 @@ $(document).ready(function() {
     lastId = $('.addRow').last()[0].id;
     console.log(lastId);
     counter = Number(lastId.replace('button-', ''))
-    console.log(lastId);
-    console.log(counter);
 
     function addRow(event) {
         console.log(event.currentTarget.id);
         dpos = event.currentTarget.id.replace('button-', '');
         counter++
         newRowId = counter
-        console.log("Adding a row after d.pos " + dpos + " ...");
-        $('#data-' + dpos).after('<li id="data-' + newRowId + '"><input type="text" size="3" name="type-' + newRowId + '"> <input type="text" size="16" name="value-' + newRowId + '"> <button class="addRow" id="button-' + newRowId + '"><strong>+</strong></button></li>');
-        buttonId = '#button-' + newRowId;
+        console.log("Adding a row after d.pos " + dpos + " with counter " + counter + " ...");
+        $('#data-' + dpos).after('<li id="data-' + counter + '"><input type="text" size="3" name="type-' + counter + '"> <input type="text" size="16" name="value-' + counter + '"> <button class="addRow" id="button-' + counter + '"><strong>+</strong></button></li>');
+        buttonId = '#button-' + counter;
         console.log("Registering the event on id " + buttonId);
         $(buttonId).click(function(ev) { ev.preventDefault(); addRow(ev); });
     }
