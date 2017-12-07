de77151f4ec37fe8602cfe7fe872db63b38c4209
Arthur Reutenauer <arthur@reutenauer.eu>
2017-12-07 23:59:00 +0100

    Renamed add row buttons.

--- sdl/talgoxe/static/talgoxe.js
+++ sdl/talgoxe/static/talgoxe.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
     lastId = $('.addRow').last()[0].id;
     console.log(lastId);
-    counter = Number(lastId.replace('button-', ''))
+    counter = Number(lastId.replace('add-row-', ''))
 
     function addRow(event) {
         console.log(event.currentTarget.id);
-        dpos = event.currentTarget.id.replace('button-', '');
+        dpos = event.currentTarget.id.replace('add-row-', '');
         counter++
         newRowId = counter
         console.log("Adding a row after d.pos " + dpos + " with counter " + counter + " ...");
-        $('#data-' + dpos).after('<li id="data-' + counter + '"><input type="text" size="3" name="type-' + counter + '"> <input type="text" size="16" name="value-' + counter + '"> <button class="addRow" id="button-' + counter + '"><strong>+</strong></button></li>');
-        buttonId = '#button-' + counter;
+        $('#data-' + dpos).after('<li id="data-' + counter + '"><input type="text" size="3" name="type-' + counter + '"> <input type="text" size="16" name="value-' + counter + '"> <button class="addRow" id="add-row-' + counter + '"><strong>+</strong></button></li>');
+        buttonId = '#add-row-' + counter;
         console.log("Registering the event on id " + buttonId);
         $(buttonId).click(function(ev) { ev.preventDefault(); addRow(ev); });
     }
@@ -18,8 +18,8 @@ $(document).ready(function() {
     function submitOrder(event) {
         ids = [];
         $('.addRow').each(function(event, data) {
-            ids.push(data.id.replace('button-', ''));
-            console.log(data.id.replace('button-', ''));
+            ids.push(data.id.replace('add-row-', ''));
+            console.log(data.id.replace('add-row-', ''));
         });
         console.log(ids.join());
         $('#spara').after('<input type="hidden" name="order" value="' + ids.join() + '">');
