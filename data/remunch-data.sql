d5638d50f3b8b7f0ab54e6f038e791b223f37433
Arthur Reutenauer <arthur@130.238.143.30>
2018-01-20 10:36:58 +0100

    Anmärkning.

--- data/remunch-data.sql
+++ data/remunch-data.sql
@@ -3,5 +3,5 @@ CREATE TABLE talgoxe_type (id INTEGER PRIMARY KEY AUTO_INCREMENT, abbrev VARCHAR
 CREATE TABLE talgoxe_data (id INTEGER PRIMARY KEY AUTO_INCREMENT, d VARCHAR(2000), pos INTEGER, lemma_id INTEGER, type_id INTEGER, CONSTRAINT FOREIGN KEY (lemma_id) REFERENCES talgoxe_lemma (id), CONSTRAINT FOREIGN KEY (type_id) REFERENCES talgoxe_type (id));
 
 INSERT INTO talgoxe_lemma (id, lemma) SELECT l_id, lemma FROM lemma WHERE l_id > 0;
-INSERT INTO talgoxe_type (id, abbrev, name) SELECT nr, fork, namn FROM typer WHERE nr > 0;
+INSERT INTO talgoxe_type (id, abbrev, name) SELECT nr, fork, namn FROM typer WHERE nr > 0; /* id = 0 är besvärligt! (SO) */
 INSERT INTO talgoxe_data (id, d, pos, lemma_id, type_id) SELECT d_id, d, pos, l_id, typ FROM fdata WHERE typ IN (SELECT nr FROM typer) AND d_id > 0 AND l_id IN (SELECT l_id FROM lemma);
