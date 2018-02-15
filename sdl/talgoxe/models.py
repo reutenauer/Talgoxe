a5d3be14f7dd6be6e1a9dd29371a1ad9a2291397
Arthur Reutenauer <arthur@130.238.143.30>
2018-02-15 13:57:40 +0100

    Bloody tabs ...

--- sdl/talgoxe/models.py
+++ sdl/talgoxe/models.py
@@ -192,7 +192,7 @@ class Lemma(models.Model):
             self.lemma = stickord
             self.save()
         d = [[post_data['type-' + key].strip(), post_data['value-' + key].strip()] for key in order]
-	gtype = Type.objects.get(abbrev = 'g')
+        gtype = Type.objects.get(abbrev = 'g')
         for i in range(len(d)):
             bit = d[i]
             try:
@@ -201,10 +201,10 @@ class Lemma(models.Model):
 # TODO >>> Type.objects.create(abbrev = 'OG', name = 'Ogiltig', id = 63)
                 type = Type.objects.get(abbrev = 'OG')
             text = bit[1]
-	    if type == gtype and text in Landskap.short_abbrev.keys():
-	      print("Got " + text + "! Normalising ...")
-	      text = Landskap.short_abbrev[text]
-	      print("  ... to " + text)
+            if type == gtype and text in Landskap.short_abbrev.keys():
+              print("Got " + text + "! Normalising ...")
+              text = Landskap.short_abbrev[text]
+              print("  ... to " + text)
             data = self.raw_data_set().filter(pos = i).first()
             if data:
                 data.type = type
