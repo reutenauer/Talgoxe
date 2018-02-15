f41e48a554ba8c3c3533d8013d35fde149ab89e3
Arthur Reutenauer <arthur@130.238.143.30>
2018-02-15 14:34:00 +0100

    Fixing that too.

--- sdl/talgoxe/urls.py
+++ sdl/talgoxe/urls.py
@@ -8,8 +8,8 @@ stickord_regexp = ur'^stickord/(?P<stickord>[.M?, TSOHF2a-zåäö()-]+)'
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url('create', views.create, name = 'create'),
-    url(ur'^(?P<id>\d+)$', views.artikel, name = 'artikel'),
+    url(r'^(?P<id>\d+)$', views.artikel, name = 'artikel'),
     url(stickord_regexp, views.artikel_efter_stickord, name = 'stickord'),
-    url(ur'^(?P<id>\d+)/print', views.print_artikel, name = 'print_artikel'),
+    url(r'^(?P<id>\d+)/print', views.print_artikel, name = 'print_artikel'),
     url(r'^print$', views.print_lexicon, name = 'printing'),
 ]
