a53c3e3a90b0e976114f664ac7fb5f3eec70e0c5
Arthur Reutenauer <arthur.reutenauer@sprakochfolkminnen.se>
2018-05-18 17:32:29 +0200

    Inte det heller.

--- talgoxe/views.py
+++ talgoxe/views.py
@@ -284,11 +284,11 @@ def search(request):
     try:
         sök_överallt = request.GET['sök-överallt']
     except MultiValueDictKeyError:
-        return render_template(request, template, { 'sök-överallt' : None, 'uri' : uri })
+        return sök_överallt : None
     spolar = Data.objects.filter(d__contains = söksträng)
     lemmata = sorted(list(OrderedDict.fromkeys([spole.lemma for spole in spolar])), key = lambda x: x.lemma)
     count = spolar.count()
-    context = { 'q' : söksträng, 'lemmata' : lemmata, 'titel' : '%d sökresultat för ”%s”' % (len(lemmata), söksträng) }
+    context = { 'q' : söksträng, 'lemmata' : lemmata, 'titel' : '%d sökresultat för ”%s”' % (len(lemmata), söksträng), 'sök_överallt' : sök_överallt }
 
     return render_template(request, template, context)
 
