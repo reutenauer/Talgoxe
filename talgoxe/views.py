91143c3d4828c8ac872ac283205b94b0ba144a39
Arthur Reutenauer <arthur@reutenauer.eu>
2018-05-23 18:43:04 +0200

    Fixat: ”abbrev” nämndes om till ”kod” i spolar.

--- talgoxe/views.py
+++ talgoxe/views.py
@@ -82,7 +82,7 @@ def artikel(request, id):
     # lemma.resolve_pilcrow()
     lemma.collect()
     if len(lemma.raw_data_set()) == 0:
-        ok = Typ.objects.get(abbrev = 'OK')
+        ok = Typ.objects.get(kod = 'OK')
         d = Spole(typ_id = ok.id, text = '', pos = 0)
         input = [d]
     else:
